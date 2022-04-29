33b4d6c0f921cad38a448fd1bb763fb1ae5c7ad8
lucks <takuto.yoshikai@gmail.com>
2022-04-29 15:10:44 +0900

    qrcode

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 function onChange() {
   document.getElementById("name").innerText = document.getElementById("name-input").value;
   document.getElementById("job").innerText = document.getElementById("job-input").value;
+  const url = document.getElementById("url").value;
   html2canvas(document.getElementById("name-section"),{
     onrendered: function(canvas){
       var imgData = canvas.toDataURL();
       document.getElementById("download").href = imgData;
     }
+  });
+  html2canvas(document.getElementById("qr-section"),{
+    onrendered: function(canvas){
+      var imgData = canvas.toDataURL();
+      document.getElementById("download-ura").href = imgData;
+    }
+  });
+  document.getElementById("qrcode").innerHTML = "";
+  new QRCode(document.getElementById("qrcode"), {
+    text: url,
+    width: 300,
+    height: 300,
+    colorLight: "#f8f8f8",
   });
 }
 
